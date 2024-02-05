b8c72e9153192d86a6001b83975f40d057918f29
Bok Chol <bokcholbok@gmail.com>
2024-02-05 13:53:25 +0000

    cureRate for Gazera state

---
+++ sudan_health_nutrition_5.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+totalNAd=sum(cmam$New.Admissions)
+totalCured=sum(cmam$Cured)
+totalDisch=sum(cmam$Total.Discharge)
+
+totalCureRate=totalCured/totalDisch
+
+##Calculation of cure rate per state ----
+#Gazera state-----
+
+
+gazera <- cmam_routine_data %>% filter(State == 'Gazera')
+gazera
+
+TotalNAd_Gazera <-sum(gazera$`New Admissions`)
+totalcured_gazera <- sum(gazera$Cured)
+total_disch_gazera <- sum(gazera$`Total Discharge`)
+
+cureRate_Gazera <- totalcured_gazera/total_disch_gazera
