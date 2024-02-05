a1cb8b6166f678d055d35a1366ec1f7122daf03e
Mariano Ratto <mratto.ib@gmail.com>
2024-02-05 12:36:34 +0000

    Non Response

--- sudan_health_nutrition_5.R
+++ sudan_health_nutrition_5.R
@@ -2,6 +2,10 @@ totalNAd=sum(cmam$New.Admissions)
 totalCured=sum(cmam$Cured)
 totalDisch=sum(cmam$Total.Discharge)
 
+totalNonResponseRate=sum(cmam$Non.Responder)/totalDisch
 totalCureRate=totalCured/totalDisch
+
+
+
 
 
