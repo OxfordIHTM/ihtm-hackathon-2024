2ac63f2c4d7196f7fb0b5c20816331c69780cc7d
Mariano Ratto <mratto.ib@gmail.com>
2024-02-07 23:50:17 +0000

    Added Analysis of Indicators for all States together

--- sudan_health_nutrition_5.R
+++ sudan_health_nutrition_5.R
@@ -1,9 +1,78 @@
-totalNAd=sum(cmam$New.Admissions)
-totalCured=sum(cmam$Cured)
-totalDisch=sum(cmam$Total.Discharge)
+cmam <- read.csv("data/cmam_routine_data.csv")
 
-totalNonResponseRate=sum(cmam$Non.Responder)/totalDisch
-totalCureRate=totalCured/totalDisch
+cmam$time = match(cmam$Month,month.abb) + (cmam$Year-2016)*12
+
+IndicatorsTotal = data.frame(
+  Names = c("Cure Rate", "Default Rate" , "Death Rate" , "Non Responder Rate", "Admitted Rate"),
+  Values = c(sum(cmam$Cured)/sum(cmam$Total.Discharge),
+             sum(cmam$Default)/sum(cmam$Total.Discharge),
+             sum(cmam$Death)/sum(cmam$Total.Discharge),
+             sum(cmam$Non.Responder)/sum(cmam$Total.Discharge),
+             sum(cmam$New.Admissions)/sum(cmam$Screening,na.rm=TRUE)
+  )
+)
+IndicatorsTotal
+
+# Not working. Trying different thing
+
+# IndicatorsbyTime = data.frame(
+#   Names = c("Time", "Cure Rate", "Default Rate" , "Death Rate" , "Non Responder Rate", "Admitted Rate"),
+#   for (i in 1:max(cmam$time)){
+#     aux=filter(cmam, time ==i)
+#     Values = c(i, 
+#                sum(aux$Cured)/sum(aux$Total.Discharge),
+#                sum(aux$Default)/aux(aux$Total.Discharge),
+#                sum(aux$Death)/sum(aux$Total.Discharge),
+#                sum(aux$Non.Responder)/sum(aux$Total.Discharge),
+#                sum(aux$New.Admissions)/sum(aux$Screening,na.rm=TRUE))
+#   }
+#     
+#   
+# )
+
+
+IndicatorsbyTime <- data.frame(
+  Time = integer(),
+  CureRate = numeric(),
+  DefaultRate = numeric(),
+  DeathRate = numeric(),
+  NonResponderRate = numeric(),
+  AdmittedRate = numeric()
+)
+
+for (i in 1:max(cmam$time)) {
+  aux <- filter(cmam, time == i)
+  
+  # Calculate each indicator
+  cureRate = sum(aux$Cured, na.rm = TRUE) / sum(aux$Total.Discharge, na.rm = TRUE)
+  defaultRate = sum(aux$Default, na.rm = TRUE) / sum(aux$Total.Discharge, na.rm = TRUE)
+  deathRate = sum(aux$Death, na.rm = TRUE) / sum(aux$Total.Discharge, na.rm = TRUE)
+  nonResponderRate = sum(aux$Non.Responder, na.rm = TRUE) / sum(aux$Total.Discharge, na.rm = TRUE)
+  if(sum(aux$Screening, na.rm = TRUE)!=0)
+    admittedRate = sum(aux$New.Admissions, na.rm = TRUE) / sum(aux$Screening, na.rm = TRUE)
+  else 
+    admittedRate = NA
+  
+  IndicatorsbyTime <- rbind(IndicatorsbyTime, c(i, cureRate, defaultRate, deathRate, nonResponderRate, admittedRate))
+}
+names(IndicatorsbyTime) <- c("Time", "CureRate", "DefaultRate", "DeathRate", "NonResponderRate", "AdmittedRate")
+
+
+
+IndicatorsLong <- pivot_longer(IndicatorsbyTime, 
+                               cols = -Time, 
+                               names_to = "RateType", 
+                               values_to = "Value")
+
+ggplot(IndicatorsLong, aes(x = Time, y = Value, color = RateType)) +
+  geom_line() + 
+  geom_point() + 
+  theme_minimal() + 
+  labs(title = "Rates for all States by Time",
+       x = "Time in month since Jan 2016",
+       y = "Rate Value",
+       color = "Rate Type") + 
+  scale_color_brewer(palette = "Set1")
 
 
 
