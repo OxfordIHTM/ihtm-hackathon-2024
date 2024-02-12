b095ba88ad2a998b0eedc0f6442462f9537d9065
Kapil Narain <kapiln.infinity@gmail.com>
2024-02-12 12:50:15 +0000

    na_table created

---
+++ sudan_health_nutrition_3.R
@@ -0,0 +1,31 @@
+
+View(child)
+dim(child)
+
+
+#################Create a table of na values per vairable####################
+
+# create a loop to determine number of na values per variabel
+na_list<-c()
+for (i in 1:ncol(child)) {
+  na_count<- sum(is.na(child[,i])) 
+  print(na_count)
+  na_list<-c(na_list,na_count)
+}
+
+colname_list<-colnames(child)
+
+##create a table of na values
+na_tbl <- data.frame(colname_list, na_list)
+
+##naming cols
+colnames(na_tbl) = c("variable", "values missing") 
+
+##calc percentage of na variables
+na_tbl[,3]<-round( na_tbl$`values missing`/nrow(child) *100,2)
+
+
+##naming cols
+colnames(na_tbl) = c("variable", "values missing","%")
+
+View(na_tbl)
