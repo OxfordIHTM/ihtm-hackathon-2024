363278bb00d3286fe0794c96b03091d1b9007593
Bok Chol <bokcholbok@gmail.com>
2024-02-12 11:52:36 +0000

    death_rate and cure_rate for Gazera state

--- sudan_health_nutrition_5.R
+++ sudan_health_nutrition_5.R
@@ -1,23 +1,18 @@
 library(tidyverse)
 library(ggplot2)
 
-cmam_routine_data
-totalNAd=sum(cmam$New.Admissions)
-totalCured=sum(cmam$Cured)
-totalDisch=sum(cmam$Total.Discharge)
-
-totalCureRate=totalCured/totalDisch
+cmam_routine_data <- read.csv(file=
+"https://raw.githubusercontent.com/OxfordIHTM/ihtm-hackathon-2024/main/data/cmam_routine_data.csv",
+sep=",",header=TRUE)
 
 ##Calculation of cure rate per state ----
 #Gazera state-----
 
-
 gazera <- cmam_routine_data %>% filter(State == 'Gazera')
-gazera
 
-TotalNAd_Gazera <-sum(gazera$`New Admissions`)
+TotalNAd_Gazera <-sum(gazera$New.Admissions)
 totalcured_gazera <- sum(gazera$Cured)
-total_disch_gazera <- sum(gazera$`Total Discharge`)
+total_disch_gazera <- sum(gazera$Total.Discharge)
 death_gazera <- sum(gazera$Death)
 
 cureRate_Gazera <- totalcured_gazera/total_disch_gazera
