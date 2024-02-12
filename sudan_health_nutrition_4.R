dc0dfb47b4b401ac08f5b3cf6a96b64c8efaf9b4
kelechichima13 <kelechi.chima@ndm.ox.ac.uk>
2024-02-12 13:18:58 +0000

    latest

--- sudan_health_nutrition_4.R
+++ sudan_health_nutrition_4.R
@@ -24,6 +24,12 @@ classify_underweight_child <- function(waz) {
   )
 }
 
+
+
+# Create a new variable to classify undernutrition based on MUAC
+maternal <- maternal %>%
+  mutate(nutrition_status = ifelse(muac < 220, 1, 0))
+
 #Adding a new column to the child data frame
 
 child$underweight_class <- classify_underweight_child(child$waz)
@@ -40,6 +46,20 @@ get_perc = function(x){
 }
 
 child_map <- child_map %>% group_by(state_name) %>% mutate(percentages = (get_perc(n)))
+
+##Naemi data
+# Create a new variable to classify undernutrition based on MUAC
+maternal <- maternal %>%
+  mutate(nutrition_status = ifelse(muac < 220, 1, 0))
+
+# Group mothers by state_name
+state_summary <- maternal %>%
+  group_by(state_name) %>%
+  summarise(
+    total_mothers = n(),
+    undernourished_mothers = sum(nutrition_status == 1, na.rm = TRUE), # Count undernourished mothers
+    percentage_undernourished = ifelse(total_mothers == 0, 0, (undernourished_mothers / total_mothers) * 100)
+  )
 
 
 ##Retrieving map data
