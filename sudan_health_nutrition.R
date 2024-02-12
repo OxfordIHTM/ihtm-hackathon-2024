3096b1d07ba5e2519d714f9a15c8819152ca46b1
Kapil Narain <kapiln.infinity@gmail.com>
2024-02-12 12:47:58 +0000

    na table created

--- sudan_health_nutrition.R
+++ sudan_health_nutrition.R
@@ -9,7 +9,7 @@ for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 ## Read data ----
 maternal <- read.csv("data/maternal_health.csv")
 child <- read.csv("data/child_health.csv")
-cmam <- read.csv("data/cmam_routine_data.csv")
+viewcmam <- read.csv("data/cmam_routine_data.csv")
 
 ### Retrieve and read Sudan map data ----
 
@@ -29,3 +29,33 @@ source("sudan_health_nutrition_4.R")
 source("sudan_health_nutrition_5.R")
 source("sudan_health_nutrition_6.R")
 
+View(child)
+dim(child)
+
+
+#################Create a table of na values per vairable####################
+
+# create a loop to determine number of na values per variabel
+na_list<-c()
+for (i in 1:ncol(child)) {
+na_count<- sum(is.na(child[,i])) 
+print(na_count)
+na_list<-c(na_list,na_count)
+}
+
+colname_list<-colnames(child)
+
+##create a table of na values
+na_tbl <- data.frame(colname_list, na_list)
+
+##naming cols
+colnames(na_tbl) = c("variable", "values missing") 
+
+##calc percentage of na variables
+na_tbl[,3]<-round( na_tbl$`values missing`/nrow(child) *100,2)
+
+
+##naming cols
+colnames(na_tbl) = c("variable", "values missing","%")
+
+View(na_tbl)
