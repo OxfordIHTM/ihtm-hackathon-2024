9cd841bbcc6b91958ef60e4f744999b41e538999
kelechichima13 <kelechi.chima@ndm.ox.ac.uk>
2024-02-13 07:17:33 +0000

    Team hinata code harmonised

--- sudan_health_nutrition_4.R
+++ sudan_health_nutrition_4.R
@@ -1,19 +1,7 @@
-#library(tidyverse)
-#library(dplyr)
-#library(ggthemes)
-#library(ggplot2)
-#library(sf)
-#library(openxlsx)   
-#library(tidyr)       
-#library(rmarkdown)   
-#library(remotes)     
-#library(here)     
-#library(tidyr)     
-#library(rmarkdown)    
+#######classifying children and mothers nutrition status#######
 
-#functions for classifying underweight children
-#Underweight is defined as the children with weight-for-age Z-score (WAZ)
-#<−2SD and severe underweight is defined as the children with WAZ <−3SD
+
+#Classifying underweight children
 
 classify_underweight_child <- function(waz) {
   ifelse(
@@ -24,20 +12,54 @@ classify_underweight_child <- function(waz) {
   )
 }
 
-
-
-# Create a new variable to classify undernutrition based on MUAC
-maternal <- maternal %>%
-  mutate(nutrition_status = ifelse(muac < 220, 1, 0))
-
-#Adding a new column to the child data frame
+#Adding a new underweight status column to the child data frame
 
 child$underweight_class <- classify_underweight_child(child$waz)
 
-#grouping underweight classes by locality
+#classifying stunted children
+classify_stunting_child <- function(haz) {
+  ifelse(
+    haz < -3, "severe stunting",
+    ifelse(
+      haz < -2, "stunting", "normal"
+    )
+  )
+}
+
+#Adding a new stunting status column to the child data frame
+child$stunting_status <- classify_stunting_child(child$haz)
+
+#classifying wasted children
+
+classify_wasting_child <- function(whz, muac, oedema) {
+  ifelse(whz < -2 | muac < 125 | oedema == 1, "wasting", "normal")
+}
+
+#Adding a new stunting status column to the child data frame
+child$wasting <- classify_wasting_child(child$whz, child$muac, child$oedema)
+
+#classifying undernourished mothers
+
+classify_undernut_mother <- function(muac) {
+  ifelse(
+   muac < 220, "undernourished","normal")
+}
+
+# Create a new variable to classify mother's nutrition status based on MUAC
+maternal$undernut <- classify_undernut_mother(maternal$muac)
+
+
+
+#########calculating percentage malnourishment by state########
+
+
+
 ##starting by counting the values of underweight by state
 
-child_map <- child %>% group_by(state_name,underweight_class) %>% count() %>% ungroup() 
+child_map_underweight <- child %>% group_by(state_name,underweight_class,state_id) %>% count() %>% ungroup() 
+child_map_stunting <- child %>% group_by(state_name,stunting_status,state_id) %>% count() %>% ungroup() 
+child_map_wasting <- child %>% group_by(state_name,wasting,state_id) %>% count() %>% ungroup() 
+maternal_map_undernourished <- maternal %>% group_by(state_name, undernut, state_id) %>% count() %>% ungroup()
 
 ##Getting a percentage from the previous count
 get_perc = function(x){
@@ -45,24 +67,13 @@ get_perc = function(x){
   percentage
 }
 
-child_map <- child_map %>% group_by(state_name) %>% mutate(percentages = (get_perc(n)))
-
-##Naemi data
-# Create a new variable to classify undernutrition based on MUAC
-maternal <- maternal %>%
-  mutate(nutrition_status = ifelse(muac < 220, 1, 0))
-
-# Group mothers by state_name
-state_summary <- maternal %>%
-  group_by(state_name) %>%
-  summarise(
-    total_mothers = n(),
-    undernourished_mothers = sum(nutrition_status == 1, na.rm = TRUE), # Count undernourished mothers
-    percentage_undernourished = ifelse(total_mothers == 0, 0, (undernourished_mothers / total_mothers) * 100)
-  )
+child_map_underweight <- child_map_underweight %>% group_by(state_id) %>% mutate(undernut_percentages = (get_perc(n)))
+child_map_stunting <- child_map_stunting %>% group_by(state_id) %>% mutate(stunting_percentages = (get_perc(n)))
+child_map_wasting <- child_map_wasting %>% group_by(state_id) %>% mutate(wasting_percentages = (get_perc(n)))
+maternal_map_undernourished <- maternal_map_undernourished %>% group_by(state_id) %>% mutate(undernut_percentages = get_perc(n))
 
 
-##Retrieving map data
+########Retrieving map data#############
 
 sudan_map_spec <- download_sudan_maps(download_url = "https://data.humdata.org/dataset/a66a4b6c-92de-4507-9546-aa1900474180/resource/e5ef3cc7-f105-4565-8d73-e08bb756f1c1/download/sdn_adm_cbs_nic_ssa_20200831.gdb.zip")
 
@@ -70,37 +81,44 @@ sudan0 <- st_read(dsn = sudan_map_spec$dsn, layer = sudan_map_spec$layers[1])
 sudan1 <- st_read(dsn = sudan_map_spec$dsn, layer = sudan_map_spec$layers[2])
 sudan2 <- st_read(dsn = sudan_map_spec$dsn, layer = sudan_map_spec$layers[4])
 
-##merging map data with the child_map data frame
+#######merging map data with the data frames###############
 
-?merge.data.frame()
-merged_childmap_data <- merge.data.frame(sudan1, child_map, by.x = "admin1Name_en", by.y = "state_name", all.x = TRUE)
+merged_childmap_underweight <- merge.data.frame(sudan1, child_map_underweight, by.x = "stateID", by.y = "state_id", all.x = TRUE)
+merged_childmap_stunting <- merge.data.frame(sudan1, child_map_stunting, by.x = "stateID", by.y = "state_id", all.x = TRUE)
+merged_childmap_wasting <- merge.data.frame(sudan1, child_map_wasting, by.x = "stateID", by.y = "state_id", all.x = TRUE)
+merged_maternalmap_undernourished <- merge.data.frame(sudan1, maternal_map_undernourished, by.x = "stateID", by.y = "state_id", all.x = TRUE)
 
-#plotting map data
+########plotting map data################
+
+#underweight children
 ggplot() +
-  geom_sf(data = merged_childmap_data, aes(geometry = Shape, fill = percentages))+
-  scale_fill_gradient(name = "Percentage of Undernourished Children", low = "lightblue", high = "darkblue", na.value = "gray50") +
-  labs(title = "Percentage of Undernourished Children by States") +
+  geom_sf(data = merged_childmap_underweight, aes(geometry = geom, fill = undernut_percentages))+
+  scale_fill_gradient(name = "Percentage of  Children", low = "lightblue", high = "darkblue", na.value = "gray50") +
+  labs(title = "Percentage of underweight Children by States") +
   theme_minimal()
 
 
-##This code below was me trying to work around the NA values, still working on it
-##merged_childmap_data1 <- merged_childmap_data  merged_childmap_data[complete.cases(merged_childmap_data$underweight_class),]
-
-
-#I don't think the map is quite right, below I'm trying to create a new data frame with a percentage
-#column with only the underweight and NA value
-
-?subset
-map_merged_childmap <- subset(merged_childmap_data, underweight_class != "normal" | is.na(underweight_class))
-
+#stunted children
 ggplot() +
- geom_sf(data = map_merged_childmap, aes(geometry = Shape, fill = percentages))+
-  scale_fill_gradient(name = "Percentage of Undernourished Children", low = "lightblue", high = "darkblue", na.value = "gray50") +
-  labs(title = "Percentage of Undernourished Children by States") +
+  geom_sf(data = merged_childmap_stunting, aes(geometry = geom, fill = stunting_percentages))+
+  scale_fill_gradient(name = "Percentage of stunted Children", low = "lightblue", high = "darkblue", na.value = "gray50") +
+  labs(title = "Percentage of stunted Children by States") +
   theme_minimal()
 
-#Starting to handle missing data
-sum(is.na(merged_childmap_data$underweight_class))
+#wasted children
+ggplot() +
+  geom_sf(data = merged_childmap_wasting, aes(geometry = geom, fill = wasting_percentages))+
+  scale_fill_gradient(name = "Percentage of wasted Children", low = "lightblue", high = "darkblue", na.value = "gray50") +
+  labs(title = "Percentage of wasted Children by States") +
+  theme_minimal()
+
+#undernourished mothers
+ggplot() +
+  geom_sf(data = merged_maternalmap_undernourished, aes(geometry = geom, fill = undernut_percentages))+
+  scale_fill_gradient(name = "Percentage of undernourished mothers", low = "lightgreen", high = "darkgreen", na.value = "gray50") +
+  labs(title = "Percentage of undernourished mothers by States") +
+  theme_minimal()
+
 
 #Trying the add state initials to the the states in the map
 ## your_dataset$new_variable <- substr(your_dataset$existing_variable, 1, 2)
