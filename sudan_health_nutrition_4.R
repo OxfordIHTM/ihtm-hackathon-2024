e2ea1f8f9532f5881582035fa19a75c3eb709e90
Ernest Guevarra <ernest@guevarra.io>
2024-02-22 21:18:32 +0000

    tidy up hinata branch

--- sudan_health_nutrition_4.R
+++ sudan_health_nutrition_4.R
@@ -1,58 +1,45 @@
 #######classifying children and mothers nutrition status#######
 
-
-#Classifying underweight children
-
-classify_underweight_child <- function(waz) {
-  ifelse(
-    waz <= -2  , "underweight",
-    ifelse(
-      waz <= -3, "severe underweight", "normal"
-    )
-  )
-}
-
 #Adding a new underweight status column to the child data frame
-
 child$underweight_class <- classify_underweight_child(child$waz)
-
-#classifying stunted children
-classify_stunting_child <- function(haz) {
-  ifelse(
-    haz < -3, "severe stunting",
-    ifelse(
-      haz < -2, "stunting", "normal"
-    )
-  )
-}
 
 #Adding a new stunting status column to the child data frame
 child$stunting_status <- classify_stunting_child(child$haz)
 
-#classifying wasted children
-
-classify_wasting_child <- function(whz, muac, oedema) {
-  ifelse(whz < -2 | muac < 125 | oedema == 1, "wasting", "normal")
-}
-
 #Adding a new stunting status column to the child data frame
 child$wasting <- classify_wasting_child(child$whz, child$muac, child$oedema)
 
-#classifying undernourished mothers
+# Create a new variable to classify children based on wasting status
+child_wasting <- child %>%
+  mutate(wasting_status = ifelse(whz < -2, 1, 0))
 
-classify_undernut_mother <- function(muac) {
-  ifelse(
-   muac < 220, "undernourished","normal")
-}
+# Group children by state_name and calculate % wasted children per state
+state_summary <- child_wasting %>%
+  group_by(state_name) %>%
+  summarise(
+    total_child = n(),                                       # Count total children
+    wasted_child = sum(wasting_status == 1, na.rm = TRUE),   # Count wasted children
+    percentage_undernourished = ifelse(total_child == 0, 0, (wasted_child / total_child) * 100)
+  )
 
 # Create a new variable to classify mother's nutrition status based on MUAC
 maternal$undernut <- classify_undernut_mother(maternal$muac)
 
+# Create a new variable to classify undernutrition based on MUAC
+maternal <- maternal %>%
+  mutate(nutrition_status = ifelse(muac < 220, 1, 0))
+
+# Group mothers by state_name
+state_summary <- maternal %>%
+  group_by(state_name) %>%
+  summarise(
+    total_mothers = n(),
+    undernourished_mothers = sum(nutrition_status == 1, na.rm = TRUE), # Count undernourished mothers
+    percentage_undernourished = ifelse(total_mothers == 0, 0, (undernourished_mothers / total_mothers) * 100)
+  )
 
 
 #########calculating percentage malnourishment by state########
-
-
 
 ##starting by counting the values of underweight by state
 
@@ -61,25 +48,11 @@ child_map_stunting <- child %>% group_by(state_name,stunting_status,state_id) %>
 child_map_wasting <- child %>% group_by(state_name,wasting,state_id) %>% count() %>% ungroup() 
 maternal_map_undernourished <- maternal %>% group_by(state_name, undernut, state_id) %>% count() %>% ungroup()
 
-##Getting a percentage from the previous count
-get_perc = function(x){
-  percentage <- round(x/sum(x)*100,1)
-  percentage
-}
-
 child_map_underweight <- child_map_underweight %>% group_by(state_id) %>% mutate(undernut_percentages = (get_perc(n)))
 child_map_stunting <- child_map_stunting %>% group_by(state_id) %>% mutate(stunting_percentages = (get_perc(n)))
 child_map_wasting <- child_map_wasting %>% group_by(state_id) %>% mutate(wasting_percentages = (get_perc(n)))
 maternal_map_undernourished <- maternal_map_undernourished %>% group_by(state_id) %>% mutate(undernut_percentages = get_perc(n))
 
-
-########Retrieving map data#############
-
-sudan_map_spec <- download_sudan_maps(download_url = "https://data.humdata.org/dataset/a66a4b6c-92de-4507-9546-aa1900474180/resource/e5ef3cc7-f105-4565-8d73-e08bb756f1c1/download/sdn_adm_cbs_nic_ssa_20200831.gdb.zip")
-
-sudan0 <- st_read(dsn = sudan_map_spec$dsn, layer = sudan_map_spec$layers[1])
-sudan1 <- st_read(dsn = sudan_map_spec$dsn, layer = sudan_map_spec$layers[2])
-sudan2 <- st_read(dsn = sudan_map_spec$dsn, layer = sudan_map_spec$layers[4])
 
 #######merging map data with the data frames###############
 
