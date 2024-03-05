51c1a247637fcac1b822cae17146c34d0c1a7eee
Ernest Guevarra <ernest@guevarra.io>
2024-03-05 14:50:10 +0000

    tidy up

--- packages.R
+++ packages.R
@@ -8,13 +8,13 @@ library(ggplot2)      ## Data visualisation using tidy approach
 library(rmarkdown)    ## Literate programming and report generation
 library(remotes)      ## For installing packages from GitHub
 library(here)         ## Utility package for working directory helper
-library(plotly)
-library(tidyr)
+library(plotly)       ## For interactive plots
+library(patchwork)    ## for composing plots into a single plot
 
 
 ## Load packages for mapping ----  
 library(sf)           ## Main package for mapping
-library(patchwork)
+
 
 ## Load packages for modelling ----
 library(lme4)         ## for regression
