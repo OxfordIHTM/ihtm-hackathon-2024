2b06e5ab02b825ba84ef7a4e899e6306643e29a7
Claudia Lucia Vidal Cuellar <claudia.vidal.c@upch.pe>
2024-03-05 11:07:27 +0000

    updated

--- sudan_health_nutrition_3.R
+++ sudan_health_nutrition_3.R
@@ -15,23 +15,13 @@ library(Publish)
 
 # GENERAL CHARACTERISTICS 
 library(gtsummary)
-tbl_summary(child_health)
 tbl_summary(child_health, include = c(accessEducation, accessBasicEducation, everAttendedSchool))
 
 # INDIVIDUAL FACTORS 
 tbl_summary(child_health, include = c(age, sex), by = accessEducation)
+tbl_summary(child_health, include = c(age, sex), by = accessBasicEducation)
+tbl_summary(child_health, include = c(age, sex), by = accessBasicEducation)
 
-#Health-related factors 
-
-child <- child %>%
-  mutate(everAttendedSchool = factor(everAttendedSchool, labels = c("No", "Yes")), 
-         safe_water = factor(vaccineRecord, labels = c("No", "Yes")))
-
-child %>%
-  drop_na(everAttendedSchool, vaccineRecord) %>%
-  tabyl(everAttendedSchool, vaccineRecord) 
-
-child %>%
-    cross_tbl(child, by = "everAtendedSchool")
-
+# HEALTH-RELATED FACTORS 
+tbl_summary(child_health, include = c(vaccineRecord, diarrhoea), by = accessEducation)
 
