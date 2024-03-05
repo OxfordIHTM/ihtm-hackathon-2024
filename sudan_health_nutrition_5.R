150c4caa829532eff32e1e1975e4cfd1663ecb79
josephine Ndawula <jojondawula363@gmail.com>
2024-03-05 10:27:34 +0000

    .

--- sudan_health_nutrition_5.R
+++ sudan_health_nutrition_5.R
@@ -212,7 +212,53 @@ ggplot(IndicatorsByYear, aes(x = Year, y = defaulters)) +
 #Create the charts by year
 
 
-print("hello i am jojo")
+#create column for quarters
+cmam <- cmam %>% mutate(Quarter = ceiling(as.numeric(cmam$time) / 3))
+
+
+#Calculate Cure rate total in Data Frame
+Cure_Rate_Total = data.frame(
+  Names = c("Cure Rate"),
+  Values = c(sum(cmam$Cured)/sum(cmam$Total.Discharge)
+  )
+)
+
+#Create Empty Indicators by a Data Frame using quarters
+Cure_Rate_by_Quarter <- data.frame(
+  Quarter = integer(),
+  Cure_Rate = numeric()
+)
+
+#Filling the Data Frame
+for (i in 1:max(cmam$Quarter)) {
+  aux <- filter(cmam,Quarter==i)
+  
+  # Calculate each indicator
+  Cure_Rate = sum(aux$Cured, na.rm = TRUE) / sum(aux$Total.Discharge, na.rm = TRUE)
+  
+  Cure_Rate_by_Quarter <- rbind(Cure_Rate_by_Quarter, c(i, Cure_Rate))
+}
+#Changing the name of columns
+names(Cure_Rate_by_Quarter) <- c("Quarter", "Cure_Rate")
+
+#Changing from short to long for plotting
+IndicatorsLong <- pivot_longer(Cure_Rate_by_Quarter, 
+                               cols = -Quarter, 
+                               names_to = "RateType", 
+                               values_to = "Value")
+#Plotting
+ggplot(IndicatorsLong, aes(x = Quarter, y = Value, color = RateType)) +
+  geom_line() + 
+  geom_point() + 
+  theme_minimal() + 
+  labs(title = "Cure Rate as a proxy for responsiveness of CMAM",
+       # x = "Time in quarters since Jan 2016",
+       y = "Cure Rate",
+       color = "Rate Type") + 
+  scale_color_brewer(palette = "Set1") +
+  scale_x_continuous(breaks = c(4, 8, 12, 16), labels = c("Dec-16", "Dec-17", "Dec-18", "Dec-19" ))
+# scale_x_discrete(labels=c('Dec-16', 'Dec-17', 'Dec-18', 'Dec-19'))
+
 
 
 #Nei Part
