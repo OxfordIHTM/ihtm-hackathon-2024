e1b66b60008874a5565424d162b6ca56387d37ee
goodgoodhead <goodgoodhead@gmail.com>
2024-03-05 06:57:57 +0000

    so far

--- sudan_health_nutrition_1.R
+++ sudan_health_nutrition_1.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(RColorBrewer)
+library(dplyr)
 
 
 maternal_health<-read.table("data/maternal_health.cvs",header = T, sep = ",")
@@ -24,6 +25,36 @@ percent_anc<-(anc4/93424)*100
 ##any IFA
 anyIFA<-sum(maternal_health$anyIFA=="1")
 percent_anyIFA<-(anyIFA/93424)*100
+
+unique(maternal_health$state_name)
+##calculate these statistics for each state--------------
+calculate_statistic <-function(maternal_health){
+  
+  stats_by_state<-maternal_health %>% 
+    group_by(state_name) %>% 
+  summarise(
+    median_gest_age= median(ancGestAge, na.rm=TRUE),
+    median_attendance=median(visitsANC, na.rm=TRUE),
+    percent_anc4= sum(anc4=="1",na.rm = TRUE)/n()*100,    
+    percent_anyIFA= sum(anyIFA=="1",na.rm = TRUE)/n()*100, 
+    percent_IFA90= sum(ifa90=="1",na.rm = TRUE)/n()*100, 
+    percent_tetanus= sum(tetanusDoses=="1",na.rm = TRUE)/n()*100    
+  )
+  return(stats_by_state)
+ 
+}
+stats_by_state<-calculate_statistic(maternal_health)
+
+select(state_name,locality_name)
+
+##group by state
+
+
+
+View(starwars)
+pratice<-starwars %>% 
+  select(height,mass)
+mutate
 
 
 
