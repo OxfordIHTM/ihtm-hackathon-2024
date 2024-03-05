30a50fbd6a2a22f9355795214cb54f1544fd9518
Clifford Cofie <cofieacademic@gmail.com>
2024-03-05 10:16:39 +0000

    update

--- sudan_health_nutrition_1.R
+++ sudan_health_nutrition_1.R
@@ -41,6 +41,42 @@ calculate_statistics <- function(maternal_data){
 
 
 
+}
+
+stats_by_state <- calculate_statistics(maternal)
+
+# Define the data for each column as separate vectors
+state_name <- c("Al-Gadarif", "Al-Gazeera", "Blue Nile", "Central Darfur", "East Darfur",
+                "Kassala", "Khartoum", "North Darfur", "North Kourdofan", "Northern",
+                "Red Sea", "River Nile", "Sinar", "South Darfur", "South Kourdofan",
+                "West Darfur", "West Kourdofan", "White Nile")
+median_gest_age <- c(3, 3, 3, 3, 4, 4, 3, 4, 3, 2, 5, 3, 3, 3, 4, 3, 4, 3)
+median_attendance <- c(3, 4, 3, 0, 2, 3, 6, 3, 4, 5, 2, 3, 3, 3, 4, 3, 3, 3)
+percent_anc4 <- c(35.9, 52.1, 39.2, 23.5, 33.8, 47.0, 77.7, 34.9, 55.7, 74.6,
+                  28.6, 47.3, 46.4, 40.9, 54.3, 37.6, 35.5, 48.9)
+percent_anyIFA <- c(72.2, 68.5, 64.2, 43.5, 54.8, 83.1, 84.5, 65.9, 80.6, 90.3,
+                    70.7, 65.8, 77.3, 47.7, 70.5, 56.3, 65.4, 81.8)
+percent_IFA90 <- c(44.9, 44.5, 36.6, 23.1, 12.0, 57.6, 72.3, 25.6, 48.3, 73.6,
+                   47.0, 46.0, 49.3, 14.1, 28.4, 19.2, 19.8, 61.1)
+
+# Create a data frame using the data
+state_data <- data.frame(
+  state = state_name,
+  anc4 = percent_anc4,
+  anyIFA = percent_anyIFA,
+  IFA90 = percent_IFA90
+)
+
+##reshapetable
+# Reshape the data into a long format
+state_data_long <- state_data %>%
+  pivot_longer(cols = c(anc4, anyIFA, IFA90), names_to = "Variable", values_to = "Value")
+
+  
+  ggplot(state_data_long, aes(x = Variable, y = Value)) +
+    geom_bar(stat = "identity") +
+    labs(title = paste("Bar Plot for", state_name), x = "Variable", y = "Value")+
+    facet_wrap()
 
 
 
